b6919c51ec5cb6d856a3e5f705b8028a43450b33
Braindead <2136249+braindead1@users.noreply.github.com>
2019-05-04 08:09:52 +0200

    Code cleanup

--- www/js/script.js
+++ www/js/script.js
@@ -1,12 +1,68 @@
-$(document).ready(function () {
-    //initPage();
+// Settings
+var connectionLink = location.origin;
 
-    setInterval(function(){
-        // Uhrzeit setzen
-        date = new Date();
-        $('#time').html(('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ' Uhr');
-    }, 1000);
-});
+var config = [];
+var states = [];
+
+
+// Websocket
+servConn._useStorage = false;
+
+connOptions = {
+    name: 'HomeHub',  // optional - default 'vis.0'
+    connLink: connectionLink,  // optional URL of the socket.io adapter
+    socketSession: '' // optional - used by authentication
+};
+connCallbacks = {
+    onConnChange: function (isConnected) {
+        if (isConnected) {
+            console.log('connected');
+
+            servConn.getObject('system.adapter.homehub.0', false, function (error, obj) {
+                console.log(JSON.stringify(obj));
+
+                config['categories'] = obj['native']['categories']; 
+                
+                initPage();
+            });
+
+            servConn.getStates(function (err, _states) {
+                var count = 0;
+                for (var id in _states) {
+                    count++;
+
+                    updateState(id, _states[id]);
+                }
+                console.log('Received ' + count + ' states.');
+                states = _states;
+
+                $('#loading').hide();
+                $('#wrapper').show();
+            });
+        } else {
+            console.log('disconnected');
+        }
+    },
+    onRefresh: function () {
+        window.location.reload();
+    },
+    onUpdate: function (id, state) {
+        setTimeout(function () {
+            states[id] = state;
+
+            updateState(id, state);
+        }, 0);
+    },
+    onError: function (err) {
+        window.alert(_('Cannot execute %s for %s, because of insufficient permissions', err.command, err.arg), _('Insufficient permissions'), 'alert', 600);
+    }
+};
+
+
+
+
+
+
 
 function initPage(_objects) {
     var i = 0;
@@ -62,8 +118,6 @@ function initPage(_objects) {
         $('#sidebar a').on("click", function() {
             var id = $(this).attr('id').replace('nav', 'content');
 
-            //console.log(id);
-
             $('#sidebar a').removeClass('selected');
             $('#content>div').hide();
 
@@ -73,19 +127,11 @@ function initPage(_objects) {
     });
 
     $('#sidebar a').first().trigger('click');
-
-    // Uhrzeit setzen
-    date = new Date();
-    $('#time').html(('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ' Uhr');
 }
 
 function updateState(id, state) {
     if(id.includes('$') === true || id.includes('%') === true || state === null) {
         return;
-    }
-
-    if(id === 'hm-rpc.1.NEQ1833684.1.STATE') {
-        console.log(id + " --> " + JSON.stringify(state));
     }
 
     selector = '.' + id.replace(/\./gi, '\\.');
@@ -96,3 +142,16 @@ function updateState(id, state) {
 
     $(selector).html(value).parent().show();
 }
+
+
+// jQuery events
+$(document).ready(function () {
+    // Uhrzeit setzen
+    setInterval(function(){
+        date = new Date();
+        $('#time').html(('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ' Uhr');
+    }, 1000);
+
+    // Websocket aufbauen
+    servConn.init(connOptions, connCallbacks);
+});
