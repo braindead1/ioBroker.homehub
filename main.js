074ca8eb2a3d37f7247e898403e4361bd88e1ab1
Braindead <2136249+braindead1@users.noreply.github.com>
2019-05-05 20:21:03 +0200

    Code cleanup

--- main.js
+++ main.js
@@ -7,18 +7,12 @@ var adapterName    = require(__dirname + '/package.json').name.split('.').pop();
 
 var utils          = require('@iobroker/adapter-core'); // Get common adapter utils
 var adapter        = new utils.Adapter(adapterName);
-var fs             = require('fs');
-var path           = require('path');
-
-var configJsPath = path.join(__dirname, "www/config.js");
 
 adapter.on('ready', function () {
     main();
 });
 
 function main() {
-    var data = "var config = ";
-
     if (adapter.config.categories === undefined || adapter.config.categories === '') {
         adapter.log.error('Configuration is missing');
 
